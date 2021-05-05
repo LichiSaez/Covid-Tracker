c578f2d38cd7390a14892bfcf6e11ff912d549cc
Lisandro Saez <lisandrosaezamelio@gmail.com>
2021-05-04 23:50:00 -0300

    4/5

--- covid-tracker/src/components/Cuadros/Cuadros.jsx
+++ covid-tracker/src/components/Cuadros/Cuadros.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchDailyData } from "../../api";
+import { Line, Bar } from "react-chartjs-2";
+import styles from "./Cuadros.module.css";
 
 const Cuadros = () => {
-  return <h1>Cuadros</h1>;
+  const [dailyData, setDailyData] = useState([]);
+
+  useEffect(() => {
+    const traerAPI = async () => {
+      setDailyData(await fetchDailyData());
+    };
+
+    traerAPI();
+  });
+
+  const cuadroLinear = dailyData.length ? (
+    <Line
+      data={{
+        labels: dailyData.map(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData.map(({ confirmed }) => confirmed),
+            label: "Infectados",
+            borderColor: "#3333ff",
+            fill: true,
+          },
+          {
+            data: dailyData.map(({ deaths }) => deaths),
+            label: "Infectados",
+            borderColor: "red",
+            backgroundColor: "rgba(255,0,0,0.5",
+            fill: true,
+          },
+        ],
+      }}
+    />
+  ) : null;
+
+  return <div className={styles.container}>{cuadroLinear}</div>;
 };
 
 export default Cuadros;
